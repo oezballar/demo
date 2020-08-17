004859dbc0529e6dc2c8708fdcbe5a0c636beade
oezballar <oezballar@rpime-reseaerch.com>
2020-08-17 22:28:15 +0200

    example donald

--- src/main/java/com/prime/service/slack/demo/Demo.java
+++ src/main/java/com/prime/service/slack/demo/Demo.java
@@ -12,23 +12,23 @@ public class Demo {
 
    @PostMapping(consumes = "application/x-www-form-urlencoded")
    public Mono<SlackResponse> processor(@ModelAttribute SlackRequest slackRequest){
-
-      String  responseText = "no text available";
+      SlackResponse response = new SlackResponse();
 
       if (slackRequest.getText() != null) {
-         responseText = slackRequest.getText();
+         if(slackRequest.getText().equals("Trump | white house | EY")){
+            response.setText("Found following disambiguation");
+            Attachment attachment = new Attachment();
+            attachment.setText("*Donald Trump* https://en.wikipedia.org/wiki/Donald_Trump");
+            attachment.setColor("#0000ff");
+            response.getAttachments().add(attachment);
+         }
+      }
+      else {
+         response.setText("disambiguation not possible");
       }
 
-      SlackResponse response = new SlackResponse();
-      response.setText( responseText );
+
       response.setResponseType("in_channel");
-
-      Attachment attachment = new Attachment();
-      attachment.setText("This is the attachment text");
-      attachment.setColor("#0000ff");
-
-      response.getAttachments().add(attachment);
-
       return Mono.just(response);
    }
 }
