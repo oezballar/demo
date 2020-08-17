5261d0983dd37b84b4e462f4a55c9b7956a9ee3d
oezballar <oezballar@rpime-reseaerch.com>
2020-08-17 15:04:54 +0200

    echos text

--- src/main/java/com/prime/service/slack/demo/Demo.java
+++ src/main/java/com/prime/service/slack/demo/Demo.java
@@ -12,11 +12,24 @@ import reactor.core.publisher.Mono;
 public class Demo {
 
    @PostMapping
-   public Mono<SlackResponse> processor(){
+   public Mono<SlackResponse> processor(@RequestParam(value = "team_id", required = false) String teamId,
+                                 @RequestParam(value = "team_domain", required = false) String teamDomain,
+                                 @RequestParam(value = "channel_id", required = false) String channelId,
+                                 @RequestParam(value = "channel_name",required = false) String channelName,
+                                 @RequestParam(value = "user_id", required = false) String userId,
+                                 @RequestParam(value = "user_name", required = false) String userName,
+                                 @RequestParam(value = "command", required = false) String command,
+                                 @RequestParam(value = "text", required = false) String text,
+                                 @RequestParam(value = "response_url", required = false) String responseUrl){
 
+      String  responseText = "no text available";
+
+      if (text != null) {
+         responseText = text;
+      }
 
       SlackResponse response = new SlackResponse();
-      response.setText("This is the response text");
+      response.setText( text );
       response.setResponseType("in_channel");
 
       Attachment attachment = new Attachment();
