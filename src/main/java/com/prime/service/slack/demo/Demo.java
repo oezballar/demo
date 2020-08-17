12d0b44d7b3a780e6b8562d4d04dd309ec5f5f0a
oezballar <oezballar@rpime-reseaerch.com>
2020-08-17 22:43:17 +0200

    example the gamer

--- src/main/java/com/prime/service/slack/demo/Demo.java
+++ src/main/java/com/prime/service/slack/demo/Demo.java
@@ -13,22 +13,43 @@ public class Demo {
    @PostMapping(consumes = "application/x-www-form-urlencoded")
    public Mono<SlackResponse> processor(@ModelAttribute SlackRequest slackRequest){
       SlackResponse response = new SlackResponse();
+      response.setResponseType("in_channel");
 
       if (slackRequest.getText() != null) {
          if(slackRequest.getText().equals("Trump | white house | EY")){
             response.setText("Found following disambiguation");
             Attachment attachment = new Attachment();
             attachment.setText("*Donald Trump* https://en.wikipedia.org/wiki/Donald_Trump");
-            attachment.setColor("#0000ff");
+            attachment.setColor("#01837E");
             response.getAttachments().add(attachment);
+            Attachment attachment2 = new Attachment();
+            attachment2.setText("*White House* https://en.wikipedia.org/wiki/White_House");
+            attachment2.setColor("#01837E");
+            response.getAttachments().add(attachment);
+            Attachment attachment3 = new Attachment();
+            attachment3.setText("*Ernest & Young* https://en.wikipedia.org/wiki/Ernst_%26_Young");
+            attachment3.setColor("#01837E");
+            response.getAttachments().add(attachment);
+            return Mono.just(response);
+         } else if (slackRequest.getText().equals("Trump | Hearthstone | Blizzard")) {
+            response.setText("Found following disambiguation");
+            Attachment attachment = new Attachment();
+            attachment.setText("*Trump (gamer)* https://en.wikipedia.org/wiki/Trump_(gamer)");
+            attachment.setColor("#01837E");
+            response.getAttachments().add(attachment);
+            Attachment attachment2 = new Attachment();
+            attachment2.setText("*Hearthstone* https://en.wikipedia.org/wiki/Hearthstone");
+            attachment2.setColor("#01837E");
+            response.getAttachments().add(attachment);
+            Attachment attachment3 = new Attachment();
+            attachment3.setText("*Blizzard Entertainment* https://en.wikipedia.org/wiki/Blizzard_Entertainment");
+            attachment3.setColor("#01837E");
+            response.getAttachments().add(attachment);
+            return Mono.just(response);
          }
       }
-      else {
-         response.setText("disambiguation not possible");
-      }
 
-
-      response.setResponseType("in_channel");
+      response.setText("disambiguation not possible");
       return Mono.just(response);
    }
 }
